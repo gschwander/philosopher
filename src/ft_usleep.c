5cf2785f9e415662c4b476a3f70a643d6b745d70
Gautier Schwander <gautier.schwander@gmail.com>
2024-10-29 18:37:01 +0100

    le probleme se trouve dans les ft_usleep surement

--- src/ft_usleep.c
+++ src/ft_usleep.c
@@ -6,7 +6,7 @@
 /*   By: gschwand <gschwand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 08:14:21 by gschwand          #+#    #+#             */
-/*   Updated: 2024/10/29 18:23:53 by gschwand         ###   ########.fr       */
+/*   Updated: 2024/10/29 18:35:49 by gschwand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,11 @@ int	ft_usleep(size_t time, t_philo *philo)
     start = get_current_time();
     while (get_current_time() - start < time)
     {
+        printf("time to sleep %zu\n", time);
+        printf("time elapsed %zu\n", get_current_time() - start);
         usleep(100);
         if (philo->dead)
-            return (1);
+            return (printf ("fuckkkkkkk\n"), 1);
     }
     return (0);
 }
