639886aeb9f0fc77f94f7f6d1a38f076fe2830dd
Gautier Schwander <gautier.schwander@gmail.com>
2024-10-29 16:45:43 +0100

    creat de la fonction time since meal

--- src/monitor.c
+++ src/monitor.c
@@ -6,13 +6,20 @@
 /*   By: gschwand <gschwand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 11:40:28 by gschwand          #+#    #+#             */
-/*   Updated: 2024/10/29 11:49:16 by gschwand         ###   ########.fr       */
+/*   Updated: 2024/10/29 16:45:14 by gschwand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
-void *monitor(t_data *data)
+int time_since_meal(t_philo *philo)
+{
+    if (get_current_time() - philo->last_meal > philo->time_to_die)
+        return (1);
+    return (0);
+}
+
+void monitor(t_data *data)
 {
     int i;
 
@@ -21,14 +28,15 @@ void *monitor(t_data *data)
         i = 0;
         while (i < data->param.nbr_of_philo)
         {
-            if (data->philo[i].eating == 0 && get_current_time() - data->philo[i].last_meal > data->philo[i].time_to_die)
+            if (!data->philo[i].eating && time_since_meal(&data->philo[i]))
             {
                 pthread_mutex_lock(data->philo[i].dead_lock);
                 data->dead_flag = 1;
-                pthread_mutex_lock(data->write_lock);
+                pthread_mutex_lock(&data->write_lock);
                 printf("%zu %d died\n", get_current_time(), data->philo[i].id);
-                pthread_mutex_unlock(data->write_lock);
+                pthread_mutex_unlock(&data->write_lock);
                 pthread_mutex_unlock(data->philo[i].dead_lock);
+                return ;
             }
             i++;
         }
